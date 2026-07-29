e29da3e77d915c6fc8d474ae2fb159c308206c66
cameliaeffendy79-stack <cameliaeffendy79@gmail.com>
2026-07-29 13:52:14 +0800

    feat(auth): implement customer register and login
    # Conflicts:
    #	backend/src/app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,17 @@
 import express from "express";
 import cors from "cors";
-
 import routes from "./routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 
-// Global Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use("/api", routes);
 
-// Global Error Handler
+
 app.use(errorMiddleware);
 
 export default app;
